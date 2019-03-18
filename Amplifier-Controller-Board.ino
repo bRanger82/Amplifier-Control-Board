d2be3a1672b025b327cdced4b16da0c34f1cc8ec
Michael <bRanger82@users.noreply.github.com>
2019-03-18 17:31:43 +0100

    Update Amplifier-Controller-Board.ino

--- Amplifier-Controller-Board.ino
+++ Amplifier-Controller-Board.ino
@@ -2,44 +2,51 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <U8g2lib.h>
+#include "splash.h"
 
+// Definition of the external connection wiring
 #define AMP_STDBY    12  // Stand-By for the Audio Amplifier
 #define RELAIS_K2    21  // Used for the relais K2
 #define RELAIS_K1    18  // Used for the relais K1
+// End of the definition of the external connection wiring
 
-// Definition of the rotary encoder connection (hardware de-bounced)
-#define RE_BUTTON    2
-#define RE_OUTPUT_A  10
-#define RE_OUTPUT_B  11
+// Definition of the rotary encoder
+#define RE_BUTTON    2   // rotary encoder button pin
+#define RE_OUTPUT_A  10  // rotary encoder 'up' movement pin
+#define RE_OUTPUT_B  11  // rotary encoder 'down' movement pin
 
-// Definition of the additional button (hardware de-bounced)
+/* 
+ *  An interrupt is used to determinate if the rotary encoder was rotated. 
+ *  The interrupt method changes the RE_STATE variable which is checked in the loop() method.
+ *  This was done to avoid doing all the work in the interrupt method.
+*/
+#define RE_STATE_UP  1   // rotary encoder was roatated 'up'
+#define RE_STATE_DW  2   // rotary encoder was roatated 'down'
+#define RE_STATE_NO  3   // rotary encoder was not roatated
+
+volatile byte RE_STATE = RE_STATE_NO; // variable for the interrupt method
+
+volatile byte ROT_ENC_BTN_PRESSED = false; // variable for the interrupt method if the rotary encoder button was pressed.
+// End of definition of the rotary encoder
+
+// Definition of the additional button
 #define EXT_BTN_ONE  22
 #define EXT_BTN_TWO  23
+// End of definition of the additional button
 
-// Definition(s) for the EEPROM
+// Definition for the EEPROM
 #define I2C_EEPROM     0x50 // --> I2C EEPROM memory address
-#define OFFSET_MEMORY    16
+#define OFFSET_MEMORY    16 // The start address for saving the I2C digital-poti values. The first 16 bytes are reserved. 
+// End of definition for the EEPROM
 
-// Definition(s) for the digital potentiometers
-#define m_steps        1   // Defines how many steps the digital potiometer has, from Low to High
-
-#define UP_STEP        5   // Difference for one step up on rotary encoder
-#define DW_STEP        5   // Difference for one step down on the rotary encoder
-#define MAX_STEP_UP  255   // Maxumum value for the digital potentiometer
-#define MIN_STEP_DW    0   // Minimum value for the digital potentiometer
-#define MAX_ITEMS      5
-
-
-
-
-byte m_wiperPosition [5] = { 0 };
-
+// Definition of the external menu items
 volatile byte ext_IDX  = 1;
 #define IDX_EXT_REL_1 1
 #define IDX_EXT_REL_2 2
 #define IDX_EXT_MAX   2
+// End of definition of the external menu items
 
-
+// Definition of menu items 
 volatile byte curr_IDX = 0;
 #define MAX_IDX 7
 #define IDX_PRE 0
@@ -51,27 +58,73 @@ volatile byte curr_IDX = 0;
 #define IDX_LOD 6
 #define IDX_EXT 7
 
-enum DSP_IDX : byte { PreVolume = 0, Volume = 1, Treble = 2, Bass = 3, Balance = 4, Save = 5, Load = 6 };  // different potentiometer types
+
+enum DSP_IDX : byte { PreVolume = IDX_PRE, Volume = IDX_VOL, Treble = IDX_TRE, Bass = IDX_BAS, Balance = IDX_BAL, Save = IDX_SAV, Load = IDX_LOD, Externals = IDX_EXT };  // different menu items
+// End of definition of menu items
+
+// Definition(s) for the digital potentiometers
+#define UP_STEP        5   // Difference for one step up on rotary encoder
+#define DW_STEP        5   // Difference for one step down on the rotary encoder
+#define MAX_STEP_UP  255   // Maxumum value for the digital potentiometer
+#define MIN_STEP_DW    0   // Minimum value for the digital potentiometer
+
+byte m_wiperPosition [5] = { 0 };
 
 // Definition of the save/load menu
-volatile byte idx_save = 1;
-volatile byte idx_load = 1;
-volatile byte idx_menu = 1;
-volatile bool ShowSave = false;
+volatile byte idx_save = 1; // saves the current position of the save menu
+volatile byte idx_load = 1; // saves the current position of the load menu
+volatile byte idx_menu = 1; // is used for the current index to be shown
+#define CNT_ITM_MAX 6
+// End of definition of the save/load menu
 
 // I2C potentiometer definitions
-#define POT_U6_W 0x28
-#define POT_U7_W 0x29
-#define POT_U8_W 0x2B
+#define POT_U6_W 0x28 // I2C address of DS18030-050
+#define POT_U7_W 0x29 // I2C address of DS18030-050
+#define POT_U8_W 0x2B // I2C address of DS18030-050
 
 #define POT_ZERO  0xA9
 #define POT_ONE   0xAA
 #define POT_BOTH  0xAF
 
-#include "splash.h"
+// End of I2C potentiometer definitions
 
-
+// Display library definition
 U8G2_SSD1327_MIDAS_128X128_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 7, /* data=*/ 5, /* cs=*/ 4, /* dc=*/ 3, /* reset=*/ 1);
+
+#define SHOW_SPLASH_TIME 5000 // shows the spash screen for 10 seconds
+// End of Display library definition
+
+// Definition for the power save mode (display only)
+volatile int LoopCount = 0;
+volatile bool DisplayIsPowerSaveMode = false;
+#define ENABLE_POWER_SAVE  1
+#define DISABLE_POWER_SAVE 0
+// End of the definition for the power save mode (display only)
+
+byte SetWiperPositionI2C(const byte, const byte, const byte);
+void DisplayText(String, String, bool = false);
+byte UpdateWiperPositionI2C(const DSP_IDX, int);
+bool IsBitSet(byte, byte);
+byte SetBit(byte, byte);
+void DisplayWiperItem(const DSP_IDX);
+void InitiateRotaryEncoder(void);
+void InitiateDisplay(void);
+void InitiateExternalButtons(void);
+void i2c_eeprom_write_byte(int, unsigned int, byte) ;
+byte i2c_eeprom_read_byte(int, unsigned int) ;
+void InitialExternalDevices(void);
+void DisplayMenu(bool, byte);
+void ProcessStep(bool, const DSP_IDX);
+void DisplayIdx(void);
+void DelayTimer(byte = 30);
+void ChangeExtValue(byte);
+void ShowExternal(byte);
+void SavePreset(byte);
+void LoadPreset(byte);
+void PrintRotaryState(void);
+void RotaryEncoderBtnPressed(void);
+void LoadDefaultPreset(void);
+void SetStandby(bool StandByOn);
 
 byte SetWiperPositionI2C(const byte PotAddr, const byte PotIdx, const byte NewValue)
 {
@@ -81,14 +134,14 @@ byte SetWiperPositionI2C(const byte PotAddr, const byte PotIdx, const byte NewVa
   Wire.write(NewValue);            // sends potentiometer value byte
   Wire.endTransmission();          // stop transmitting
 
-  // take some time to breath
+  // take some time to breath, removing this delay can cause issue -> value will not be set
   delay(5);
 
   // return the new value set
   return NewValue;
 }
 
-void DisplayText(String Text, String Value, bool SmallText = false)
+void DisplayText(String Text, String Value, bool SmallText)
 {
   u8g2.clearBuffer();
   delay(1);
@@ -127,13 +180,13 @@ byte UpdateWiperPositionI2C(const DSP_IDX dsp_idx, int Value)
   if (Value > 255)
   {
     NewValue = 255;
-  }
-  if (Value < 0)
+  } else if (Value < 0)
   {
     NewValue = 0 ;
+  } else
+  {
+    NewValue = (byte)Value;
   }
-  NewValue = (byte)Value;
-  
   byte Addr = 0;
   byte Poti = 0;
   switch (dsp_idx)
@@ -158,12 +211,9 @@ byte UpdateWiperPositionI2C(const DSP_IDX dsp_idx, int Value)
       Addr = POT_U8_W;
       Poti = POT_ONE;
       break;
-    default:
-      Addr = 0;
-      Poti = 0;
-      break;
+    default: // the DSP_IDX is not assigned to a digital potientiometer 
+      return (byte)0;
   }
-
   m_wiperPosition[dsp_idx] = SetWiperPositionI2C(Addr, Poti, NewValue);
   
   return m_wiperPosition[dsp_idx];
@@ -185,7 +235,6 @@ byte SetBit(byte b, byte pos)
 void DisplayWiperItem(const DSP_IDX idx)
 {
   String dsp = "";
-  
   switch (idx)
   {
     case PreVolume:
@@ -203,9 +252,8 @@ void DisplayWiperItem(const DSP_IDX idx)
     case Balance: 
       dsp += "Balance";
       break;
-    default:
-      dsp += "Undefined";
-      break;
+    default: // the DSP_IDX is not assigned to a digital potientiometer
+      return;
   }
   DisplayText(dsp, String(m_wiperPosition[idx]));
 }
@@ -229,8 +277,9 @@ void InitiateDisplay(void)
   u8g2.drawXBM(0, 32, splash1_width, splash1_height, splash1_data);  
   u8g2.drawStr(3, 112, "  Welcome Back  ");
   u8g2.sendBuffer();         
+  delay(10);
   u8g2.clearBuffer();
-  delay(10000);
+  delay(SHOW_SPLASH_TIME);
 }
 
 void InitiateExternalButtons()
@@ -266,32 +315,46 @@ byte i2c_eeprom_read_byte( int deviceaddress, unsigned int eeaddress )
 
 void InitialExternalDevices(void)
 {
-  pinMode(AMP_STDBY, OUTPUT); // Stand-By for the Audio Amplifier
-  pinMode(RELAIS_K2, OUTPUT); // Used for the relais K2
-  pinMode(RELAIS_K1, OUTPUT); // Used for the relais K1
-  digitalWrite(AMP_STDBY, LOW);
+  pinMode(AMP_STDBY, OUTPUT);   // Stand-By for the Audio Amplifier
+  pinMode(RELAIS_K2, OUTPUT);   // Used for the relais K2
+  pinMode(RELAIS_K1, OUTPUT);   // Used for the relais K1
+  // Setting default values
+  digitalWrite(AMP_STDBY, LOW); 
   digitalWrite(RELAIS_K1, LOW);
   digitalWrite(RELAIS_K2, LOW);
 }
 
 void setup() 
 {
+  InitialExternalDevices();
+  SetStandby(true);
   InitiateDisplay();
   InitiateRotaryEncoder();
   InitiateExternalButtons();
-  InitialExternalDevices();
-  
-  curr_IDX = 0;  // Set default index value
-
-  delay(30);
-  LoadPreset((byte)1);
-  delay(30);
+  curr_IDX = IDX_PRE;  // First item to be shown
+  LoadDefaultPreset();
   DisplayWiperItem((DSP_IDX)curr_IDX);
-  delay(30);
+  delay(10);
+  SetStandby(false);
 }
 
-#define CNT_ITM_MAX 6
+void LoadDefaultPreset(void)
+{
+  delay(10);
+  LoadPreset((byte)1); // Preset one is the default preset which is loaded from the beginning
+  delay(10);
+}
 
+void SetStandby(bool StandByOn)
+{
+  if (StandByOn)
+  {
+    digitalWrite(AMP_STDBY, HIGH);
+  } else
+  {
+    digitalWrite(AMP_STDBY, LOW);
+  }
+}
 
 void DisplayMenu(bool ShowSave, byte idxCursor)
 {
@@ -327,8 +390,9 @@ void DisplayMenu(bool ShowSave, byte idxCursor)
     u8g2.drawStr(15, 18  + (pos * 15), itm);
   }
   u8g2.sendBuffer();
+  delay(5);
   u8g2.clearBuffer();
-  delay(10);
+  delay(5);
 }
 
 void ProcessStep(bool StepUp, const DSP_IDX idx)
@@ -392,109 +456,6 @@ void ProcessStep(bool StepUp, const DSP_IDX idx)
   }
 }
 
-volatile byte RE_STATE = 0;
-
-#define RE_STATE_UP  1
-#define RE_STATE_DW  2
-#define RE_STATE_NO  3
-
-void PrintRotaryState(void)
-{
-  if (digitalRead(RE_OUTPUT_A) == LOW)
-  {
-    RE_STATE = RE_STATE_UP;
-  } else 
-  {
-    RE_STATE = RE_STATE_DW;
-  }
-}
-
-volatile byte ROT_ENC_BTN_PRESSED = false;
-void RotaryEncoderBtnPressed(void)
-{
-  ROT_ENC_BTN_PRESSED = true;
-}
-
-void loop() 
-{
-  u8g2.clearBuffer();
-  delay(10);
-  
-  if (RE_STATE == RE_STATE_DW)
-  {
-    // Rotary encoder was turned 'down'
-    ProcessStep(false, (DSP_IDX)curr_IDX);
-    RE_STATE = RE_STATE_NO;
-    return;
-  } else if (RE_STATE == RE_STATE_UP)
-  {
-    // Rotary encoder was turned 'up'
-    ProcessStep(true, (DSP_IDX)curr_IDX);
-    RE_STATE = RE_STATE_NO;
-    return;
-  }
-  
-  if (ROT_ENC_BTN_PRESSED)
-  {
-    ROT_ENC_BTN_PRESSED = false;
-    
-    if (++curr_IDX > MAX_IDX)
-    {
-      curr_IDX = IDX_PRE;
-    }
-    
-    DisplayIdx();
-    return;
-  } else if (digitalRead(EXT_BTN_ONE) == HIGH)
-  {
-    String txt = "";
-    String value = "";
-    switch(curr_IDX)
-    {
-      case IDX_SAV:
-        SavePreset(idx_save);
-        txt = "Saved";
-        value = "Preset: " + String(idx_save);
-        DisplayText(txt, value, true);
-        DelayTimer();
-        DisplayIdx();
-        break;
-      case IDX_LOD:
-        LoadPreset(idx_load);
-        txt = "Loaded";
-        value = "Preset: " + String(idx_load);
-        DisplayText(txt, value, true);
-        DelayTimer();
-        DisplayIdx();
-        break;
-      case IDX_EXT:
-        ChangeExtValue(ext_IDX);
-        ShowExternal(ext_IDX);
-        break;
-      default:
-        break;
-    }
-
-    // debouncing
-    while(digitalRead(EXT_BTN_ONE) == HIGH) { }
-  } else if (digitalRead(EXT_BTN_TWO) == HIGH)
-  {
-    if (digitalRead(AMP_STDBY) == HIGH)
-    {
-      DisplayText("Standby", "SET TO OFF", true);
-    } else
-    {
-      DisplayText("Standby", "SET TO ON", true);
-    }
-    digitalWrite(AMP_STDBY, !digitalRead(AMP_STDBY));
-    // debouncing
-    while(digitalRead(EXT_BTN_TWO) == HIGH) { }
-    DelayTimer();
-    DisplayIdx();
-    return;
-  }
-}
-
 void DisplayIdx(void)
 {
   if (curr_IDX <= 4)
@@ -515,13 +476,13 @@ void DisplayIdx(void)
     return;
   }
 }
-void DelayTimer(void)
+
+void DelayTimer(byte waitTime)
 {
-  for (byte tmr = 0; tmr <= 30; tmr++)
+  for (byte tmr = 0; tmr <= waitTime; tmr++)
   {
     delay(100);
   }
-  DisplayMenu(false, idx_load);
 }
 
 void ChangeExtValue(byte idx)
@@ -594,7 +555,7 @@ void SavePreset(byte idx)
     // Calculate the correct address for saving the value
     unsigned int startPos = (sizeof(m_wiperPosition) * idx) + OFFSET_MEMORY + b;
     i2c_eeprom_write_byte(I2C_EEPROM, startPos, m_wiperPosition[b]);
-    delay(20);
+    delay(10);
   }
 }
 
@@ -604,8 +565,134 @@ void LoadPreset(byte idx)
   {
     unsigned int startPos = (sizeof(m_wiperPosition) * idx) + OFFSET_MEMORY + b;
     byte ValueFromEEPROM = i2c_eeprom_read_byte(I2C_EEPROM, startPos);
-    delay(20);
+    delay(10);
     m_wiperPosition[b] = ValueFromEEPROM;
     UpdateWiperPositionI2C((DSP_IDX)b, m_wiperPosition[b]);
+  }
+}
+
+
+
+void PrintRotaryState(void)
+{
+  if (digitalRead(RE_OUTPUT_A) == LOW)
+  {
+    RE_STATE = RE_STATE_UP;
+  } else 
+  {
+    RE_STATE = RE_STATE_DW;
+  }
+}
+
+
+void RotaryEncoderBtnPressed(void)
+{
+  ROT_ENC_BTN_PRESSED = true;
+}
+
+
+void loop() 
+{
+  delay(10);
+  
+#ifdef POWERSAVE_MODE_ACTIVE   
+  if ((RE_STATE == RE_STATE_DW) || (RE_STATE == RE_STATE_UP) || (ROT_ENC_BTN_PRESSED) || (digitalRead(EXT_BTN_ONE) == HIGH) || (digitalRead(EXT_BTN_TWO) == HIGH))
+  {
+    if (DisplayIsPowerSaveMode)
+    {
+      RE_STATE = RE_STATE_NO;
+      u8g2.setPowerSave(DISABLE_POWER_SAVE);
+      LoopCount = 0;
+      DisplayIsPowerSaveMode = false;
+      return;
+    }
+  } else
+  {
+    LoopCount++;
+    if (LoopCount > 1000)
+    {
+      if (!DisplayIsPowerSaveMode)
+      {
+        u8g2.setPowerSave(ENABLE_POWER_SAVE);
+        DisplayIsPowerSaveMode = true;
+        return;
+      }
+      LoopCount = 0;
+    }    
+  }
+#endif
+
+  u8g2.clearBuffer();
+  
+  if (RE_STATE == RE_STATE_DW)
+  {
+    RE_STATE = RE_STATE_NO;
+    // Rotary encoder was turned 'down'
+    ProcessStep(false, (DSP_IDX)curr_IDX);
+    return;
+  } else if (RE_STATE == RE_STATE_UP)
+  {
+    RE_STATE = RE_STATE_NO;
+    // Rotary encoder was turned 'up'
+    ProcessStep(true, (DSP_IDX)curr_IDX);
+    return;
+  } else if (ROT_ENC_BTN_PRESSED)
+  {
+    // reset the button status
+    ROT_ENC_BTN_PRESSED = false;
+    
+    // idx is the current screen to be shown
+    // if the next iteration it exceeds the maximum index, the first index will be shown again
+    if (++curr_IDX > MAX_IDX)
+    {
+      curr_IDX = IDX_PRE;
+    }
+    
+    DisplayIdx();
+    return;
+  } else if (digitalRead(EXT_BTN_ONE) == HIGH)
+  {
+    switch(curr_IDX)
+    {
+      case IDX_SAV:
+        SavePreset(idx_save);
+        DisplayText("Saved", "Preset: " + String(idx_save), true);
+        DelayTimer();
+        break;
+      case IDX_LOD:
+        LoadPreset(idx_load);
+        DisplayText("Loaded", "Preset: " + String(idx_load), true);
+        DelayTimer();
+        break;
+      case IDX_EXT:
+        ChangeExtValue(ext_IDX);
+        break;
+      default:
+        break;
+    }
+    
+    DisplayIdx();
+    
+    // debouncing
+    while(digitalRead(EXT_BTN_ONE) == HIGH) { }
+    return;
+    
+  } else if (digitalRead(EXT_BTN_TWO) == HIGH)
+  {
+    if (digitalRead(AMP_STDBY) == HIGH)
+    {
+      digitalWrite(AMP_STDBY, LOW);
+      DisplayText("Standby", "SET TO OFF", true);
+    } else
+    {
+      digitalWrite(AMP_STDBY, HIGH);
+      DisplayText("Standby", "SET TO ON", true);
+    }
+    DelayTimer((byte)20);
+    DisplayIdx();
+    
+    // debouncing
+    while(digitalRead(EXT_BTN_TWO) == HIGH) { }
+    return;
   }
 }
